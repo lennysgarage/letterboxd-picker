4ba5f3076cc21170e1bf27a8ccf433bdc815983c
lennysgarage <lennysgarage1@gmail.com>
2022-05-10 13:11:38 -0400

    feat: add loading spinner for request

--- frontend/controller.js
+++ frontend/controller.js
@@ -8,26 +8,31 @@ function sendRequest() {
         let username = document.getElementById('username').value;
         if (username !== '') {
             showLoading();
-            const response = await fetch(`http://localhost:8080/api?username=${username}`);
+            // development: http://localhost:8080/api?username=${username}
+            // production: https://letterboxd-picker-api.herokuapp.com/api?username=${username}
+            const response = await fetch(`https://letterboxd-picker-api.herokuapp.com/api?username=${username}`);
             hideLoading();
+            let movie = document.getElementById("movie-container");
             if (response.status === 200) {
                 const content = await response.json();
-                let movie = document.getElementById("movie-container");
+                let movieTitle = content.title.replace(/-/g, " ");
                 movie.innerHTML = `<a 
                     class="movie-poster" 
                     href="${content.movielink}" 
-                    style="background-image: url('${content.imagelink}');"></a>`
+                    style="background-image: url('${content.imagelink}');"></a>
+                    <p id="movieTitle">You should watch <a id="movieTitleLink" href="${content.movielink}">${movieTitle.charAt(0).toUpperCase() + movieTitle.slice(1)}</a></p>`
             } else {
-                console.log(response.status)
+                movie.innerHTML = `<p id="missingMovie">Sorry that watchlist does not exist.</p>
+                <img class="not-found" src="404-not-found.gif" alt="20th century fox intro, replaced with 404 Not found error.">`;
             }
         }
     }
 }
 
 function showLoading() {
-    document.getElementById('spinner').style.display = 'block';
+    document.getElementById('submitButton').innerHTML = '<span id="spinner" class="spinner-border text-light spinner-border-sm" role="status" aria-hidden="true"></span>Loading...';
 }
 
 function hideLoading() {
-    document.getElementById('spinner').style.display = 'none';
+    document.getElementById('submitButton').innerHTML = 'SUBMIT';
 }
